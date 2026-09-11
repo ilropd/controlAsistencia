ef80492abb7250630a09235b1ddb0788121d484c
Tim <dm161272@gmail.com>
2026-09-11 14:31:01 +0200

    ready for develop 3.Actualizar registro

--- src/util/limpiar_pantalla.py
+++
@@ -1,9 +0,0 @@
-import platform
-import subprocess
-
-def limpiar_pantalla():
-    """Limpia la pantalla de la consola según el sistema operativo."""
-    if platform.system() == "Windows":
-        subprocess.run("cls", shell=True)
-    else:
-        subprocess.run(["clear"])
--- src/util/validators.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def validar_formato_hora(hora):
-    patron = r"^([01]\d|2[0-3]):([0-5]\d)$"
-    return bool(re.match(patron, hora))
-
-def validar_id_empleado(emp_id):
-    patron = r"^[A-Z]{3}\d{3}$"
-    return bool(re.match(patron, emp_id))
